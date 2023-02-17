21b415090a7b370f3c489cdfe873089d251992c7
armstjc <70788681+armstjc@users.noreply.github.com>
2023-02-17 12:17:04 -0500

    Added NASCAR ARCA race results (main, east, west)

--- parse_racing_reference.py
+++ parse_racing_reference.py
@@ -91,11 +91,23 @@ def get_racing_reference_race_results(season:int,series_id="W"):
         except:
             raise FileNotFoundError(f'Could not find a schedule file for the {season} Craftsman Truck season.')
     ## ARCA series
-    elif series_id.upper() == "C":
+    elif series_id.upper() == "A":
         try:
             sched_df = pd.read_csv(f'racing_reference/nascar_arca/schedule/{season}_schedule.csv')
         except:
             raise FileNotFoundError(f'Could not find a schedule file for the {season} ARCA season.')
+    ## ARCA East series
+    elif series_id.upper() == "P":
+        try:
+            sched_df = pd.read_csv(f'racing_reference/nascar_arca_east/schedule/{season}_schedule.csv')
+        except:
+            raise FileNotFoundError(f'Could not find a schedule file for the {season} ARCA East season.')
+    ## ARCA West series
+    elif series_id.upper() == "E":
+        try:
+            sched_df = pd.read_csv(f'racing_reference/nascar_arca_west/schedule/{season}_schedule.csv')
+        except:
+            raise FileNotFoundError(f'Could not find a schedule file for the {season} ARCA West season.')
     ## Modified series
     elif series_id.upper() == "N":
         try:
@@ -112,14 +124,14 @@ def get_racing_reference_race_results(season:int,series_id="W"):
         raise ValueError("The input for series_id is invalid.")
 
     race_url_arr = sched_df['race_url'].to_list()
-
+    race_num_arr = sched_df['race_num'].to_list()
     #main_df = pd.DataFrame()
     race_df = pd.DataFrame()
     row_df = pd.DataFrame()
 
     headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-    for i in tqdm(race_url_arr):
-        url = i
+    for i in tqdm(range(0,len(race_url_arr))):
+        url = race_url_arr[i]
         response = requests.get(url,headers=headers)
         soup = BeautifulSoup(response.text, features='lxml')
 
@@ -129,9 +141,13 @@ def get_racing_reference_race_results(season:int,series_id="W"):
         #print(f"\n{html_table}")
 
         race_id = str(url).replace("https://www.racing-reference.info/race-results/","").replace(f"/{series_id.upper()}","")
-
+        if race_num_arr[i] < 10:
+            race_id_short = f"{season}_0{race_num_arr[i]}_{series_id}"
+        else:
+            race_id_short = f"{season}_{race_num_arr[i]}_{series_id}"
+        
         for j in html_table.find_all('tr'):
-            row_df = pd.DataFrame({'season':season,'series_id':series_id,'race_id':race_id},index=[0])
+            row_df = pd.DataFrame({'season':season,'series_id':series_id,'race_id':race_id,'race_id_short':race_id_short},index=[0])
             row = j.find_all('td')
 
             if len(row) < 2:
@@ -144,7 +160,10 @@ def get_racing_reference_race_results(season:int,series_id="W"):
                 row_df['starting_position'] = row[1].text.strip()
                 row_df['driver_number'] = str(row[2].text.strip())
                 row_df['driver_nationality'] = str(row[3].find("img").get("src")).replace('//www.racing-reference.info/wp-content/themes/ndms/images/racing-reference/flags/','').replace('.png','')
-                row_df['driver_id'] = str(row[3].find("a").get("href")).replace('https://www.racing-reference.info/driver/','').replace('/','')
+                try:
+                    row_df['driver_id'] = str(row[3].find("a").get("href")).replace('https://www.racing-reference.info/driver/','').replace('/','')
+                except:
+                    row_df['driver_id'] = None
                 row_df['driver_name'] = row[3].text.strip()
                 sponsor_owner = row[4].text.strip()
                 row_df['sponsor'] = sponsor_owner.split('   ')[0]
@@ -152,24 +171,30 @@ def get_racing_reference_race_results(season:int,series_id="W"):
                     row_df['owner_id'] = str(row[4].find("a").get("href")).replace('https://www.racing-reference.info/owner/','')
                 except:
                     row_df['owner_id'] = None
-                row_df['car'] = row[5].text.strip()
+                try:                    
+                    row_df['car'] = row[5].text.strip()
+                except:
+                    row_df['car'] = None
                 row_df['race_laps_run'] = row[6].text.strip()
                 row_df['race_status'] = row[7].text.strip()
                 row_df['race_laps_lead'] = row[8].text.strip()
-                row_df['points_earned'] = row[9].text.strip()
+                try:
+                    row_df['points_earned'] = row[9].text.strip()
+                except:
+                    row_df['points_earned'] = None
                 try:
                     row_df['playoff_points_earned'] = row[10].text.strip()
                 except:
                     row_df['playoff_points_earned'] = None
                 race_df = pd.concat([race_df,row_df],ignore_index=True)
         #print(race_df)
-        time.sleep(2)
+        time.sleep(1)
 
     return race_df
 
 
 def main():
-    for i in range(1948,2024):
+    for i in range(1958,2024):
         ## Cup Series
         if i >= 1949:
             sched_df = get_racing_reference_standings(season=i,series_id="W")
@@ -217,6 +242,30 @@ def main():
                     results_df.to_csv(f"racing_reference/nascar_arca/race_results/{i}_race_results.csv",index=False)
             except:
                 print(f'Could not get race results for the {i} NASCAR ARCA season.')
+        
+        ## ARCA East Series
+        if i >= 1954:
+            sched_df = get_racing_reference_standings(season=i,series_id="P")
+            if len(sched_df) > 0:
+                sched_df.to_csv(f"racing_reference/nascar_arca_east/schedule/{i}_schedule.csv",index=False)
+            try:
+                results_df = get_racing_reference_race_results(season=i,series_id="P")
+                if len(results_df) > 0:
+                    results_df.to_csv(f"racing_reference/nascar_arca_east/race_results/{i}_race_results.csv",index=False)
+            except:
+                print(f'Could not get race results for the {i} NASCAR ARCA East season.')
+
+        ## ARCA West Series
+        if i >= 1987:
+            sched_df = get_racing_reference_standings(season=i,series_id="E") # Yes, this is the actual leauge ID for ARCA West.
+            if len(sched_df) > 0:
+                sched_df.to_csv(f"racing_reference/nascar_arca_west/schedule/{i}_schedule.csv",index=False)
+            try:
+                results_df = get_racing_reference_race_results(season=i,series_id="E")
+                if len(results_df) > 0:
+                    results_df.to_csv(f"racing_reference/nascar_arca_west/race_results/{i}_race_results.csv",index=False)
+            except:
+                print(f'Could not get race results for the {i} NASCAR ARCA West season.')
 
         ## Modified Series
         if i >= 1948:
