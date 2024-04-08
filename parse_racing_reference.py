36adad706b63f038b67492f06956721f0e5717bf
Joseph Armstrong <70788681+armstjc@users.noreply.github.com>
2024-04-08 10:55:36 -0400

    Removed Selenium, changed to Python 3.12

--- parse_racing_reference.py
+++ parse_racing_reference.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
+# from selenium import webdriver
 from tqdm import tqdm
 
 
